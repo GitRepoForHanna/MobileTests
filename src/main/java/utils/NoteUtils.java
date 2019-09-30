cd55510612df6b5c4822b1d8b590b37324176cb9
Hanna_Holubeva <Annette95@yandex.ru>
2019-09-30 14:57:15 +0300

    parameters names were refactored

--- src/main/java/utils/NoteUtils.java
+++ src/main/java/utils/NoteUtils.java
@@ -4,8 +4,8 @@ import org.apache.commons.lang3.StringUtils;
 
 public class NoteUtils {
 
-    public static String getDifferenceBetweenNoteData(String currentTitle, String newTitle) {
-        String resultStr = StringUtils.remove(newTitle.toLowerCase(), currentTitle.toLowerCase());
+    public static String getDifferenceBetweenNoteData(String currentDataStr, String newDataStr) {
+        String resultStr = StringUtils.remove(newDataStr.toLowerCase(), currentDataStr.toLowerCase());
         return resultStr.substring(0,1).toUpperCase() + resultStr.substring(1);
     }
 }
